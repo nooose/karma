4a2ab1fb6b1a137f2209c69cb8df6015e27e4494
nooose <hd15807@gmail.com>
2025-03-19 18:05:19 +0900

    chore: 원칙 메시지 추가

--- src/main/kotlin/com/karma/KarmaApplication.kt
+++ src/main/kotlin/com/karma/KarmaApplication.kt
@@ -3,10 +3,8 @@ package com.karma
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @ConfigurationPropertiesScan
-@EnableScheduling
 @SpringBootApplication
 class KarmaApplication
 
--- src/main/kotlin/com/karma/support/SchedulingConfig.kt
+++ src/main/kotlin/com/karma/support/SchedulingConfig.kt
@@ -2,9 +2,10 @@ package com.karma.support
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 
-
+@EnableScheduling
 @Configuration(proxyBeanMethods = false)
 class SchedulingConfig {
 
